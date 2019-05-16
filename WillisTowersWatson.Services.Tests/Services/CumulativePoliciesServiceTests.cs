d051e844d75d5e9ef962b3ba08345c6b0b8dc179
HQ\ven.pachipulusu <Venkatesh.Pachipulusu@peninsula-uk.com>
2019-05-16 09:24:09 +0100

    Updated Unit tests

--- WillisTowersWatson.Services.Tests/Services/CumulativePoliciesServiceTests.cs
+++ WillisTowersWatson.Services.Tests/Services/CumulativePoliciesServiceTests.cs
@@ -11,22 +11,38 @@ namespace WillisTowersWatson.Services.Tests.Services
 {
     public class CumulativePoliciesServiceTests : IDisposable
     {
-        private MockRepository mockRepository;
+        private readonly MockRepository _mockRepository;
 
 
         public CumulativePoliciesServiceTests()
         {
-            this.mockRepository = new MockRepository(MockBehavior.Strict);
+            this._mockRepository = new MockRepository(MockBehavior.Strict);
         }
 
         public void Dispose()
         {
-            this.mockRepository.VerifyAll();
+            this._mockRepository.VerifyAll();
         }
 
         private CumulativePoliciesService CreateService()
         {
             return new CumulativePoliciesService();
+        }
+
+        private string GetFullFilePath(string filePath)
+        {
+            string fullFilePath = string.Empty;
+            var directoryInfo = Directory.GetParent(Environment.CurrentDirectory).Parent;
+            if (directoryInfo?.Parent != null) fullFilePath = $"{directoryInfo.Parent.FullName}{filePath}";
+            return fullFilePath;
+        }
+
+        private void CheckFileExistAndClearContent(string fileFullPath)
+        {
+            if (File.Exists(fileFullPath))
+            {
+                File.WriteAllText(fileFullPath, string.Empty);
+            }
         }
 
         [Fact]
@@ -40,42 +56,41 @@ namespace WillisTowersWatson.Services.Tests.Services
             var result = unitUnderTest.GetCumulativePolicies(nonCumulativeViewModels);
 
             // Assert
-            Assert.True(false);
+            Assert.True(result.CumulativePoliciesViewModels.Count > 0);
         }
 
         [Theory]
-        [InlineData(@"\InputFiles\InputFile.csv")]
+        [InlineData(@"\TestFiles\InputFile.csv")]
         public void NonCumulativeInputFileReader_StateUnderTest_ExpectedBehavior(string filePath)
         {
             // Arrange
             var unitUnderTest = this.CreateService();
-
-            string path = string.Empty;
-            var directoryInfo = Directory.GetParent(Environment.CurrentDirectory).Parent;
-            if (directoryInfo?.Parent != null) path = $"{directoryInfo.Parent.FullName}{filePath}";
+            string fullFilePath = GetFullFilePath(filePath);
 
             // Act
-            var result = unitUnderTest.NonCumulativeInputFileReader(path);
+            var result = unitUnderTest.NonCumulativeInputFileReader(fullFilePath);
 
             // Assert
-            Assert.True(false);
+            Assert.True(result.Count > 0);
         }
 
-        [Fact]
-        public void CumulativeOutputFile_StateUnderTest_ExpectedBehavior()
-        {
-            //// Arrange
-            //var unitUnderTest = this.CreateService();
-            //List<NonCumulativePoliciesViewModel> nonCumulativePoliciesViewModels = TODO;
-            //string outputFilePath = TODO;
+        [Theory]
+        [InlineData(@"\TestFiles\OutputFile.csv")]
 
-            //// Act
-            //unitUnderTest.CumulativeOutputFile(
-            //    nonCumulativePoliciesViewModels,
-            //    ref outputFilePath);
+        public void CumulativeOutputFile_StateUnderTest_ExpectedBehavior(string outputFilePath)
+        {
+            // Arrange
+            var unitUnderTest = this.CreateService();
+            outputFilePath = GetFullFilePath(outputFilePath);
+            CheckFileExistAndClearContent(outputFilePath);
+            List<NonCumulativePoliciesViewModel> nonCumulativePoliciesViewModels = MockedData.GetNonCumulativePoliciesViewModels();
+
+            // Act
+            unitUnderTest.CumulativeOutputFile(nonCumulativePoliciesViewModels, ref outputFilePath);
+            int content = File.ReadAllText(outputFilePath).Length;
 
             // Assert
-            Assert.True(false);
+            Assert.True(content > 0);
         }
     }
 }
